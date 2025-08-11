4da26c6de80566737390b2b5dd740542de350dba
Kumar Mridul <135414241+Mridul-Srivastava03@users.noreply.github.com>
2025-08-12 00:54:39 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        if(nums.length == 1 && nums[0] != val)
-            return 1;
-        for(int j = 1; j < nums.length; j++){
-            if(nums[i] != val){
+        int i = 0; // pointer for placing non-val elements
+        
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
                 i++;
-                continue;
             }
-            if(nums[i] == val && nums[j] == val){
-                continue;
-            }
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
         }
         return i;
     }
